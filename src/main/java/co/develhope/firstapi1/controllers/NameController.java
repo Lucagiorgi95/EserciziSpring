3feb720e78ca8746deeeb47162fe13e2afd2e45d
Lucagiorgi95 <lucagiorgi995@gmail.com>
2023-03-07 13:25:54 +0100

    Esercizio 2

--- src/main/java/co/develhope/firstapi1/controllers/NameController.java
+++ src/main/java/co/develhope/firstapi1/controllers/NameController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Esercizio 01 di Spring
+ */
 @RestController
 public class NameController {
 
