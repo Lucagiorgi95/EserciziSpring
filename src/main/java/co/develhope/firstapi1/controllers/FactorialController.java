4c4cc7e412866135e0b2f93a0b68b47b64a64416
Lucagiorgi95 <lucagiorgi995@gmail.com>
2023-03-07 13:16:37 +0100

    Esercizio 2

---
+++ src/main/java/co/develhope/firstapi1/controllers/FactorialController.java
@@ -0,0 +1,14 @@
+package co.develhope.firstapi1.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FactorialController {
+
+    @GetMapping(value = "/factorial/{id}")
+    public String getFactorial(@PathVariable(required = false, value = "id") String id){
+      return "Questo è il tuo id" + id;
+    }
+}
